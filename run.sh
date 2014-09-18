abdc66e392c03fde0364607212dcd5a75b5bced0
André Aizim Kelmanson <akelmanson@gmail.com>
2014-09-18 16:36:42 -0300

    bugfixes

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -n "$ROLLBAR_NOTIFY_ACCESS_TOKEN" ]; then
+if [ ! -n "$WERCKER_ROLLBAR_NOTIFY_ACCESS_TOKEN" ]; then
   error 'Please specify access-token property'
   exit 1
 fi
@@ -12,7 +12,7 @@ if [ "$WERCKER_ROLLBAR_NOTIFY_ON" = "passed" ]; then
   fi
 fi
 curl https://api.rollbar.com/api/1/deploy/ \
-  -F access_token=$ROLLBAR_NOTIFY_ACCESS_TOKEN \
-  -F environment=${ROLLBAR_NOTIFY_ENVIRONMENT:-$WERCKER_DEPLOYTARGET_NAME} \
+  -F access_token=$WERCKER_ROLLBAR_NOTIFY_ACCESS_TOKEN \
+  -F environment=${WERCKER_ROLLBAR_NOTIFY_ENVIRONMENT:-$WERCKER_DEPLOYTARGET_NAME} \
   -F revision=$WERCKER_GIT_COMMIT \
-  -F local_username=${ROLLBAR_NOTIFY_USERNAME:-$WERCKER_STARTED_BY}
+  -F local_username=${WERCKER_ROLLBAR_NOTIFY_USERNAME:-$WERCKER_STARTED_BY}
